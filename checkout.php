6941deab2a8bc8d2da216ba8ee0178add64e241a
JoseUrbina <joseantonioug-09@hotmail.com>
2018-12-25 23:48:39 -0600

    Video 22: Listar los productos en checkout.php y guardalor en sesion

--- checkout.php
+++ checkout.php
@@ -6,7 +6,7 @@
 <!-- Test variable for seeing the amount of a product
      But this space will be used for checking if we are adding products
 -->
-<?php echo $_SESSION['producto_1'];?>
+<?php // echo $_SESSION['producto_1'];?>
 
     <!-- Page Content -->
 <div class="container">
@@ -31,22 +31,85 @@
     <table class="table table-striped">
         <thead>
           <tr>
-           <th>Product</th>
-           <th>Price</th>
-           <th>Quantity</th>
+           <th>Producto</th>
+           <th>Precio</th>
+           <th>Cantidad</th>
            <th>Sub-total</th>
      
           </tr>
         </thead>
         <tbody>
+          <?php
+              try
+              {
+                  $conectar = Conectar::conexion();
+
+                  // A way to foreach all stored SESSIONS
+                  foreach($_SESSION as $name => valor){
+                      // Validacion para listar solo productos con una cantidad > 0
+                      if($valor > 0)
+                      {
+                        // Verify if session name it's a product
+                        if(substr($name, 0 , 9) == "producto_")
+                        {
+                            // Variable which stores the length to use
+                            // in the next substr function
+                            $length = strlen($name) - 9;
+
+                            // Getting the id_product from the session name
+                            // 9: Represent the space after _
+                            $id_producto = substr($name, 9, $length);  
+
+                            $sql = "SELECT * FROM productos WHERE id_producto = :id_producto";
+
+                            $resultado = $conectar->prepare($sql);
+
+                            if($resultado->execute())
+                            {
+                                while($reg = $resultado->fetch(PDO::FETCH_ASSOC)){
+                                    $id_producto = $reg["id_producto"];
+                                    $producto_titulo = $reg["producto_titulo"];
+                                    $producto_precio = $reg["producto_precio"]; 
+                                    $producto_cantidad = $reg["producto_cantidad"];
+                                    $producto_imagen = $reg["producto_imagen"]; 
+                                } // Cierre while
+                            } // Cierre IF Conditional
+                            else
+                            {
+                              echo "<h1 style='color:red;'>Failed query</h1>";
+                            }
+                        } // Cierre de la validacion substr
+                      } // Cierre validacion del valor > 0
+                  } // Cierre del foreach
+              }
+              catch(Exception $e)
+              {
+                die("Error: {$e->getMessage()}");
+              }
+              finally{
+                $conectar = null;
+              }
+          ?>
             <tr>
                 <td>apple</td>
                 <td>$23</td>
                 <td>3</td>
                 <td>2</td>
-                <td><a href="carro.php?remover=1">Remover</a></td>
-                <td><a href="carro.php?eliminar=1">Eliminar</a></td>
+                <td>
+                    <a class="btn btn-warning" href="carro.php?remover=<?php echo $id_producto;?>">
+                    <span class="glyphicon glyphicon-minus"></span></a>
+
+                    <a class="btn btn-success" href="carro.php?agregar=<?php echo $id_producto;?>">
+                    <span class="glyphicon glyphicon-plus"></span></a>
+
+                    <a class="btn btn-danger" href="carro.php?eliminar=<?php echo $id_producto;?>">
+                    <span class="glyphicon glyphicon-remove"></span></a>
+                </td>
             </tr>
+
+          <?php
+                    
+          ?>
         </tbody>
     </table>
 </form>
