0d4b5c0158c258ea29308ef4c753a27863b5b4ac
JoseUrbina <joseantonioug-09@hotmail.com>
2019-01-15 00:09:54 -0600

    Video 34: Creacion de la tabla reportes y insertar registros en reportes y pedidos

--- gracias.php
+++ gracias.php
@@ -14,7 +14,7 @@
 
 		/* INsertar el registro de pedidos*/
 
-		try
+		/* try
 		{
 			$conectar = Conectar::conexion();
 
@@ -50,7 +50,119 @@
 	}
 
 	// Destruir la session para volver checkout a 0
-	session_destroy();
+	//session_destroy();
+
+	*/
+	}
+?>
+
+<?php
+
+	try
+	{
+		// A way to foreach all stored SESSIONS
+          foreach($_SESSION as $name => $valor){
+              // Validacion para listar solo productos con una cantidad > 0
+              if($valor > 0)
+              {
+                // Verify if session name it's a product
+                if(substr($name, 0 , 9) == "producto_")
+                {
+                    // Variable which stores the length to use
+                    // in the next substr function
+                    $length = strlen($name) - 9;
+
+                    // Getting the id_product from the session name
+                    // 9: Represent the space after _
+                    $id_producto = substr($name, 9, $length);
+
+                    /***** INSERTAR PEDIDO INICIO *****/
+
+                    $conectar = Conectar::conexion();
+
+					$sql = "INSERT INTO pedidos(pedido_amount, pedido_transaction,
+												pedido_status, pedido_currency) 
+							VALUES(:amount, :transaction, :status, :currency);";
+
+					$resultado = $conectar->prepare($sql);
+
+					$resultado->bindValue(":amount", $amount);
+					$resultado->bindValue(":transaction", $transaction);
+					$resultado->bindValue(":status", $status);
+					$resultado->bindValue(":currency", $currency);
+
+					if($resultado->execute())
+					{
+						/* echo "<h1 class='text-center text-success'>It has done a new pedido record</h1>"; */
+
+						$ultimo_id_pedido = $conectar->lastInsertId();
+					}
+					else
+					{
+						echo "<h1 class='text-danger text-center'>Failed query<h1>";
+					}
+
+                    /***** INSERTAR PEDIDO FIN *****/
+
+                    // SELECCIONA LOS PRODUCTOS EN CHECKOUT.PHP
+
+                    $sql = "SELECT * FROM productos WHERE id_producto = :id_producto";
+
+                    $resultado = $conectar->prepare($sql);
+                    $resultado->bindValue(":id_producto", $id_producto);
+
+                    if($resultado->execute())
+                    {
+                        while($reg = $resultado->fetch(PDO::FETCH_ASSOC)){
+                            $producto_titulo = $reg["producto_titulo"];
+                            $producto_precio = $reg["producto_precio"]; 
+                            $producto_cantidad = $reg["producto_cantidad"];
+
+                        /***** INSERTAR REPORTE INICIO *****/
+
+                        $insertar_reporte = "INSERT INTO reportes(id_producto, id_pedido, producto_precio, producto_titulo, producto_cantidad) 
+                        	VALUES(:id_producto, :id_pedido, :p_precio, :p_titulo, :p_cantidad);";
+
+                       	$resultado = $conectar->prepare($insertar_reporte);
+
+                       	$resultado->bindValue(":id_producto", $id_producto);
+                       	$resultado->bindValue(":id_pedido", $ultimo_id_pedido);
+                       	$resultado->bindValue(":p_precio", $producto_precio);
+                       	$resultado->bindValue(":p_titulo", $producto_titulo);
+                       	// Valor : cantidad a comprar de un determinado producto
+                       	$resultado->bindValue(":p_cantidad", $valor);
+
+                       	if(!$resultado->execute())
+                       	{
+                       		echo "<h1 class='text-danger'>Failed query</h1>";
+                       	}
+
+                        /***** INSERTAR REPORTE FIN *****/
+
+                        } // Cierre while
+
+                    } // Cierre IF Conditional
+                    else
+                    {
+                        echo "<h1 style='color:red;'>Failed query</h1>";
+                    }
+                } // Cierre de la validacion substr
+            } // Cierre validacion del valor > 0
+        } // Cierre del foreach
+
+        /*
+        	Una vez que se inserten los pedido y reportes se destruye la session para evitar que se hagan nuevos registros al refrescar la pagina gracias.php con los parametros
+        */
+        session_destroy();
+	}
+	catch(Exception $e)
+	{
+		die("Error: {$e->getMessage()}");
+	}
+	finally{
+		$conectar = null;
+	}
+
 ?>
 
 <div class="container">
